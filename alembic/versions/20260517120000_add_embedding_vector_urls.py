56a71779c35fd5dc8792e05b8ea1518a15b0c411
hai2411-tlu <phambathuonghai2p4112004@gmail.com>
2026-05-29 15:35:33 +0700

    fix alembic

--- alembic/versions/20260517120000_add_embedding_vector_urls.py
+++ alembic/versions/20260517120000_add_embedding_vector_urls.py
@@ -14,7 +14,7 @@ from sqlalchemy import text
 
 # revision identifiers, used by Alembic.
 revision: str = '20260517120000'
-down_revision: Union[str, None] = '0f160c703113'
+down_revision: Union[str, None] = '8dd9d7da9120'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
